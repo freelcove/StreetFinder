9476cb238ad7f9e6e888bbf8c292a2b4fb9b07e6
FreeLcove <freelcove@gmail.com>
2023-06-13 12:43:41 +0900

    backend helloworld

---
+++ backend/src/main/java/com/cafelcove/streetfinder/StreetfinderApplication.java
@@ -0,0 +1,13 @@
+package com.cafelcove.streetfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetfinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cafelcove/streetfinder/StreetfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafelcove.streetfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
