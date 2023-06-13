48cac62ae1e4a1eb371733385a8c927475308588
FreeLcove <freelcove@gmail.com>
2023-06-14 00:52:21 +0900

    use env var

--- backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
+++ backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    public static class Greeting {
+        private String message;
+
+        public Greeting(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello from backend";
+    public Greeting hello() {
+        return new Greeting("Hello from backend");
     }
 }
