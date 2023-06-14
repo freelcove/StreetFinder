36a121e0915f4635d8497b28ce19de2c721b8265
byungsoo1234 <baptist0@naver.com>
2023-06-14 12:28:19 +0900

    read csv
    CSV 파일 절대경로 수정 필요

--- backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
+++ backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
@@ -1,7 +1,15 @@
 package com.cafelcove.streetfinder.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cafelcove.streetfinder.dto.TotalDTO;
+import com.cafelcove.streetfinder.csv.CsvParser;
+import com.cafelcove.streetfinder.dao.MysqlDAO;
 
 @RestController
 public class TestController {
@@ -26,4 +34,20 @@ public class TestController {
     public Greeting hello() {
         return new Greeting("Hello from backend");
     }
+	    @GetMapping(value = "/dbconnect")
+    public String dbtest(){
+        MysqlDAO mysql = new MysqlDAO();
+        return mysql.read();
+    }
+	    @GetMapping(value = "/testCSVRead")
+    public String csvTest(){
+        CsvParser csvparser = new CsvParser();
+        ArrayList<TotalDTO> data = csvparser.readCSV();
+        String result ="";
+        for (TotalDTO item : data) {
+            result+=item.toString();
+        }
+        return result;
+    }
+    
 }
