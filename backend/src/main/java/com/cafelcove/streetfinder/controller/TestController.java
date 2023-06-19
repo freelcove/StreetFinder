5f262dd1905053a708199427d784468539eea6c4
FreeLcove <freelcove@gmail.com>
2023-06-19 10:33:22 +0900

    Update TestController.java

--- backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
+++ backend/src/main/java/com/cafelcove/streetfinder/controller/TestController.java
@@ -1,19 +1,8 @@
 package com.cafelcove.streetfinder.controller;
 
-import java.util.ArrayList;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cafelcove.streetfinder.dto.CategoryDTO;
-import com.cafelcove.streetfinder.dto.CitiesDTO;
-import com.cafelcove.streetfinder.dto.PlacesDTO;
-import com.cafelcove.streetfinder.dto.SubcategoryDTO;
-import com.cafelcove.streetfinder.dto.TotalDTO;
-import com.cafelcove.streetfinder.csv.CsvParser;
-import com.cafelcove.streetfinder.dao.MysqlDAO;
 
 @RestController
 public class TestController {
