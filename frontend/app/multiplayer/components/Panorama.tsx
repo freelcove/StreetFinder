5fb8fee81e649f78863ad04ee117ed214cc4902e
장대건 <jdg8988@naver.com>
2023-06-26 13:30:07 +0900

    compass update

--- frontend/app/multiplayer/components/Panorama.tsx
+++ frontend/app/multiplayer/components/Panorama.tsx
@@ -1,68 +1,69 @@
 "use client";
-import { MultiplayerGameContext } from '../context/MultiplayerGameContext';
-import { useEffect, useRef, useState, useContext } from 'react';
+import { MultiplayerGameContext } from "../context/MultiplayerGameContext";
+import { useEffect, useRef, useState, useContext } from "react";
 
 export default function Panorama() {
-    const panoRef = useRef<HTMLDivElement>(null);
-    const { coordinates, photodate, setPhotodate } = useContext(MultiplayerGameContext);
-    const [panorama, setPanorama] = useState(null);
+  const panoRef = useRef<HTMLDivElement>(null);
+  const { coordinates, photodate, setPhotodate } = useContext(
+    MultiplayerGameContext,
+  );
+  const [panorama, setPanorama] = useState(null);
 
-    const initPano = () => {
-        if (!window.naver.maps.Panorama || !coordinates) {
-            console.error('Naver Maps Panorama script or coordinates are not loaded.');
-            return;
-        }
-        const panoOptions = {
-            position: new window.naver.maps.LatLng(coordinates.lat, coordinates.lng),
-            pov: {
-                pan: -135,
-                tilt: 0,
-                fov: 120,
-            },
-            flightSpot: false,
-            logoControl: false,
-            zoomControl: false,
-            aroundControl: false,
-        };
-        setPanorama(new window.naver.maps.Panorama(panoRef.current, panoOptions));
+  const initPano = () => {
+    if (!window.naver.maps.Panorama || !coordinates) {
+      console.error(
+        "Naver Maps Panorama script or coordinates are not loaded.",
+      );
+      return;
+    }
+    const panoOptions = {
+      position: new window.naver.maps.LatLng(coordinates.lat, coordinates.lng),
+      pov: {
+        pan: -135,
+        tilt: 0,
+        fov: 120,
+      },
+      flightSpot: false,
+      logoControl: false,
+      zoomControl: false,
+      aroundControl: false,
     };
+    setPanorama(new window.naver.maps.Panorama(panoRef.current, panoOptions));
+  };
 
-    useEffect(() => {
-        initPano();
+  useEffect(() => {
+    initPano();
+  }, []);
 
-    }, []);
+  useEffect(() => {
+    if (panorama && coordinates) {
+      const position = new window.naver.maps.LatLng(
+        coordinates.lat,
+        coordinates.lng,
+      );
+      panorama.setPosition(position);
+      const timer = setTimeout(() => {
+        setPhotodate(panorama.getLocation().photodate);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [coordinates]);
 
-    useEffect(() => {
-        if (panorama && coordinates) {
-            const position = new window.naver.maps.LatLng(coordinates.lat, coordinates.lng);
-            panorama.setPosition(position);
-            const timer = setTimeout(() => {
-                setPhotodate(panorama.getLocation().photodate);
-            }, 1000);
-            return () => clearTimeout(timer);
-        }
-    }, [coordinates]);
+  useEffect(() => {
+    // window 크기에 따라 화면 resize
+    const handleResize = () => {
+      let size = { width: window.innerWidth, height: window.innerHeight };
+      if (panorama) panorama.setSize(size);
+    };
+    window.addEventListener("resize", handleResize);
 
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (panorama) {
+        panorama.destroy();
+      }
+    };
+  }, [panorama]);
 
-    useEffect(() => {
-        // window 크기에 따라 화면 resize
-        const handleResize = () => {
-            let size = { width: window.innerWidth, height: window.innerHeight }
-            if (panorama) panorama.setSize(size);
-        }
-        window.addEventListener('resize', handleResize);
-
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            if (panorama) {
-                panorama.destroy();
-            }
-        };
-    }, [panorama]);
-
-
-    return (
-        <div ref={panoRef} className="w-full h-full z-0" />
-    );
-};
+  return <div ref={panoRef} className="w-full h-full z-0" />;
+}
--- frontend/app/singleplayer/components/Panorama.tsx
+++ frontend/app/singleplayer/components/Panorama.tsx
@@ -1,73 +1,129 @@
-"use client";
-
-import { useEffect, useRef, useState, useContext } from 'react';
-import { GameContext } from '../context/GameContext';
+import { useEffect, useRef, useState, useContext } from "react";
+import { GameContext } from "../context/GameContext";
 
 export default function Panorama() {
-    const panoRef = useRef<HTMLDivElement>(null);
-    const {coordinates} = useContext(GameContext);
-    const [panorama, setPanorama] = useState(null);
+  const panoRef = useRef<HTMLDivElement>(null);
+  const { coordinates } = useContext(GameContext);
+  const [panorama, setPanorama] = useState(null);
+  const [panoramaRotation, setPanoramaRotation] = useState(0);
+  const [isMouseDown, setIsMouseDown] = useState(false);
 
-    const initPano = () => {
-        if (!window.naver.maps.Panorama || !coordinates) {
-            console.error('Naver Maps Panorama script is not loaded.');
-            return;
-        }
-        const panoOptions = {
-            position: new window.naver.maps.LatLng(coordinates.lat, coordinates.lng),
-            pov: {
-                pan: -135,
-                tilt: 25,
-                fov: 100,
-            },
-            flightSpot: false,
-            logoControl: false,
-            zoomControl: false,
-            aroundControl: false,
-
-
-
-
-        };
-        setPanorama(new window.naver.maps.Panorama(panoRef.current, panoOptions));
+  const initPano = () => {
+    if (!window.naver.maps.Panorama || !coordinates) {
+      console.error("Naver Maps Panorama script is not loaded.");
+      return;
+    }
+    const panoOptions = {
+      position: new window.naver.maps.LatLng(coordinates.lat, coordinates.lng),
+      pov: {
+        pan: -135,
+        tilt: 0,
+        fov: 100,
+      },
+      flightSpot: false,
+      logoControl: false,
+      zoomControl: false,
+      aroundControl: false,
     };
 
-    useEffect(() => {
-        initPano();
-       
-    }, []);
+    setPanorama(new window.naver.maps.Panorama(panoRef.current, panoOptions));
+  };
 
-    useEffect(() => {
-        if (panorama && coordinates) {
-            const position = new window.naver.maps.LatLng(coordinates.lat, coordinates.lng);
-            panorama.setPosition(position);
-        }
-    }, [coordinates]);
+  useEffect(() => {
+    initPano();
+  }, []);
 
-    useEffect(() => {
-        const handleResize = () => {
-            let size = { width: window.innerWidth, height: window.innerHeight }
-            if (panorama) panorama.setSize(size);
-        }
-        window.addEventListener('resize', handleResize);
+  useEffect(() => {
+    if (panorama && coordinates) {
+      const position = new window.naver.maps.LatLng(
+        coordinates.lat,
+        coordinates.lng,
+      );
+      panorama.setPosition(position);
+    }
+  }, [coordinates]);
 
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [panorama]);
+  useEffect(() => {
+    const handleResize = () => {
+      let size = { width: window.innerWidth, height: window.innerHeight };
+      if (panorama) panorama.setSize(size);
+    };
+    window.addEventListener("resize", handleResize);
 
-    useEffect(() => {
-        return () => {
-            // Clean up on unmount
-            if (panorama) {
-                panorama.destroy();
-            }
-        };
-    }, [panorama]);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [panorama]);
 
+  useEffect(() => {
+    const handlePanoramaRotate = () => {
+      if (panorama && panorama.getPov) {
+        const pov = panorama.getPov();
+        setPanoramaRotation(pov.pan);
+      }
+    };
 
+    if (panorama) {
+      panorama.addListener("pov_changed", handlePanoramaRotate);
+    }
 
-    return (
-        <div ref={panoRef} className="w-full h-full z-0" />
-    );
-};
+    return () => {
+      if (panorama) {
+        panorama.removeListener("pov_changed", handlePanoramaRotate);
+      }
+    };
+  }, [panorama]);
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+    if (panorama) {
+      panorama.setOptions({ clickable: false });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+    if (panorama) {
+      panorama.setOptions({ clickable: true });
+    }
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!isMouseDown) return;
+
+    const { movementX, movementY } = event.nativeEvent;
+    const rotationSpeed = 0.2; // 파노라마 동작 속도 조절 (값이 클수록 느림)
+
+    if (panorama) {
+      const pov = panorama.getPov();
+      const newPan = pov.pan - movementX * rotationSpeed;
+      const newTilt = pov.tilt - movementY * rotationSpeed;
+      panorama.setPov({ pan: newPan, tilt: newTilt, fov: pov.fov });
+    }
+  };
+
+  return (
+    <div
+      className="w-full h-full z-0 relative"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
+      <div ref={panoRef} className="w-full h-full" />
+      <img
+        src="/image/compass.png"
+        alt="Compass"
+        className="compass-image"
+        style={{
+          transform: `rotate(${panoramaRotation}deg)`,
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          width: "50px",
+          height: "50px",
+          transition: "transform 0.3s ease-in-out",
+        }}
+      />
+    </div>
+  );
+}
